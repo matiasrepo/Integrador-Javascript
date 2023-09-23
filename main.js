c85567076d87d65e443999ecec92ea130b5dd9ed
Matias Nicolás Nuñez Gonzalez <matiasautomot@gmail.com>
2023-09-23 16:05:42 -0300

    upd

--- main.js
+++ main.js
@@ -77,7 +77,78 @@ function mostrarComics(comics) {
     });
 }
 
-// ...
+// ... (código previo)
+
+// Variables para llevar un registro de los favoritos
+const favoritos = new Set(); // Usamos un conjunto para evitar duplicados
+
+// Función para agregar o quitar un superhéroe como favorito
+function toggleFavorito(superheroe) {
+    if (favoritos.has(superheroe)) {
+        favoritos.delete(superheroe);
+    } else {
+        favoritos.add(superheroe);
+    }
+}
+
+// Función para mostrar los favoritos en el pop-up
+function mostrarFavoritos() {
+    const favoritosContainer = document.querySelector('.favoritos-container');
+    favoritosContainer.innerHTML = ''; // Limpiar el contenido anterior
+
+    favoritos.forEach(superheroe => {
+        const superheroeCard = document.createElement('div');
+        superheroeCard.classList.add('superhero-card');
+        
+        const name = document.createElement('h2');
+        name.textContent = superheroe.name;
+
+        if (superheroe.thumbnail && superheroe.thumbnail.path && superheroe.thumbnail.extension) {
+            const imageUrl = `${superheroe.thumbnail.path}.${superheroe.thumbnail.extension}`;
+            const image = document.createElement('img');
+            image.src = imageUrl;
+            superheroeCard.appendChild(image);
+        }
+        
+        superheroeCard.appendChild(name);
+
+        // Botón para quitar de favoritos
+        const removeButton = document.createElement('button');
+        removeButton.textContent = 'Quitar de Favoritos';
+        removeButton.classList.add('btn-remove-favorito');
+        removeButton.addEventListener('click', () => {
+            toggleFavorito(superheroe); // Quitar de favoritos
+            mostrarFavoritos(); // Actualizar la lista de favoritos
+        });
+        superheroeCard.appendChild(removeButton);
+
+        favoritosContainer.appendChild(superheroeCard);
+    });
+}
+
+// Mostrar el pop-up de Favoritos al hacer clic en el botón "Favoritos"
+const btnFavoritos = document.getElementById('btn-favoritos');
+btnFavoritos.addEventListener('click', () => {
+    const modalFavoritos = document.getElementById('modal-favoritos');
+    modalFavoritos.style.display = 'block';
+    mostrarFavoritos();
+});
+
+// Cerrar el pop-up de Favoritos al hacer clic en la "X"
+const btnCloseModal = document.getElementById('btn-close-modal');
+btnCloseModal.addEventListener('click', () => {
+    const modalFavoritos = document.getElementById('modal-favoritos');
+    modalFavoritos.style.display = 'none';
+});
+
+// Cargar superhéroes y cómics al cargar la página
+window.addEventListener('DOMContentLoaded', () => {
+    cargarSuperheroes();
+    cargarComics();
+});
+
+// ... (código posterior)
+
 
 
 // Cargar superhéroes y cómics al cargar la página
