cb16b231e58947f5aa5135c3c748a7906f2f9470
Matias Nicolás Nuñez Gonzalez <matiasautomot@gmail.com>
2023-09-23 15:29:14 -0300

    upd

--- main.js
+++ main.js
@@ -1,61 +1,59 @@
-const apiKey = '29993103bd6f4c459f2a845005acd9e3';
-const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
+const apiKey = '02f07ccab23a7bb1dd2e6466cccdd320641f9ad8'; // Reemplaza con tu clave de API de Marvel
+// URL base de la API de Marvel
+const apiUrl = 'https://gateway.marvel.com/v1/public/';
 
-// Función para cargar las noticias desde la API
-function cargarNoticias() {
-    fetch(apiUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('No se pudo obtener la respuesta de la API.');
-            }
-            return response.json();
-        })
+// Realizar solicitud de superhéroes
+function cargarSuperheroes() {
+    const superheroesEndpoint = `${apiUrl}characters?apikey=${apiKey}&limit=10`;
+    fetch(superheroesEndpoint)
+        .then(response => response.json())
         .then(data => {
-            if (data.articles) {
-                mostrarNoticias(data.articles);
-            } else {
-                throw new Error('No se encontraron artículos.');
+            if (data.data && data.data.results) {
+                mostrarSuperheroes(data.data.results);
             }
         })
         .catch(error => {
-            console.error('Error al cargar noticias:', error);
+            console.error('Error al cargar superhéroes:', error);
         });
 }
 
+// Realizar solicitud de cómics
+function cargarComics() {
+    const comicsEndpoint = `${apiUrl}comics?apikey=${apiKey}&limit=10`;
+    fetch(comicsEndpoint)
+        .then(response => response.json())
+        .then(data => {
+            if (data.data && data.data.results) {
+                mostrarComics(data.data.results);
+            }
+        })
+        .catch(error => {
+            console.error('Error al cargar cómics:', error);
+        });
+}
 
-// Función para mostrar las noticias en tarjetas
-function mostrarNoticias(noticias) {
-    const newsContainer = document.querySelector('.news-cards');
-
-    noticias.slice(0, 40).forEach(noticia => {
-        const newsCard = document.createElement('div');
-        newsCard.classList.add('news-card');
-
-        const title = document.createElement('h2');
-        title.textContent = noticia.title;
-
-        // Comprueba si hay una URL de imagen válida antes de crear la imagen
-        if (noticia.urlToImage) {
-            const image = document.createElement('img');
-            image.src = noticia.urlToImage;
-            newsCard.appendChild(image);
-        }
-
-        const description = document.createElement('p');
-        description.textContent = noticia.description;
-
-        const link = document.createElement('a');
-        link.href = noticia.url;
-        link.textContent = 'Leer más';
-
-        newsCard.appendChild(title);
-        newsCard.appendChild(description);
-        newsCard.appendChild(link);
-
-        newsContainer.appendChild(newsCard);
+function mostrarSuperheroes(superheroes) {
+    const superheroesContainer = document.querySelector('.superheroes-container');
+    superheroes.forEach(superheroe => {
+        const superheroeCard = document.createElement('div');
+        superheroeCard.classList.add('superhero-card');
+        superheroeCard.textContent = superheroe.name;
+        superheroesContainer.appendChild(superheroeCard);
     });
 }
 
+function mostrarComics(comics) {
+    const comicsContainer = document.querySelector('.comics-container');
+    comics.forEach(comic => {
+        const comicCard = document.createElement('div');
+        comicCard.classList.add('comic-card');
+        comicCard.textContent = comic.title;
+        comicsContainer.appendChild(comicCard);
+    });
+}
 
-// Cargar noticias al cargar la página
-window.addEventListener('DOMContentLoaded', cargarNoticias);
+// Cargar superhéroes y cómics al cargar la página
+window.addEventListener('DOMContentLoaded', () => {
+    cargarSuperheroes();
+    cargarComics();
+});
